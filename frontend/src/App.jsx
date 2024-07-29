2dc5aa8f50a5caff34cd4158def39ab8ed667f1a
Akash Pathak <akashpathak@Akashs-MacBook-Air.local>
2024-07-29 23:06:11 +0530

    update django cors header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { retreatList } from "../lib/data";
 import Filter from "./components/Filter/Filter";
 import Header from "./components/Header/Header";
 import RetreatDisplay from "./components/RetreatDisplay/RetreatDisplay";
+import axios from "axios";
 
 const App = () => {
   const [filteredEvents, setFilteredEvents] = useState(retreatList);
+  const fetchEvents = async () => {
+    const response = await axios.get('https://wellness-retreat-webapp-api.onrender.com/api/retreats/')
+    console.log(response);
+
+  }
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
   return (
     <>
